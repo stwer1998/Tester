ed7848d5e4e1d2a614c6d264261f839fce8b9d16
stwer1998 <stwer1998@mail.ru>
2020-01-01 20:51:09 +0300

    Add class

---
+++ TesterProject/TesterDb/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TesterCore.Models;
+
+namespace TesterDb
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<TestWhithCloseAns> TestWhithCloseAns { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ TesterProject/TesterDb/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using TesterCore.Models;
+
+namespace TesterDb
+{
+    public class UserRepository
+    {
+        private MyDbContext db;
+        public UserRepository(MyDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void AddUser(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public bool CheckLogin(string login)
+        {
+            var user = db.Users.FirstOrDefault(x => x.Login == login);
+            if (user == null || user.Login != login) { return false; }
+            else return true;
+        }
+
+        public User GetUser(string login, string password)
+        {
+            var user = db.Users.FirstOrDefault(x => x.Login == login && x.Password == password);
+            if (user == null || user.Login != login) { return null; }
+            else return user;
+        }
+    }
+}
